ed3d8d893637fdd3ce0fb6e11eccf7a221001156
supertask <lightfox.task@gmail.com>
2020-08-01 08:54:17 +0900

    Init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# covid19-fatality-rate-visualizer
-A fatality rate visualizer of Covid-19.
+# Covid-19 fatality rate visualizer in Japan
+I haven't seen fatality rate statistics on covid-19 websites except reserch reports.
+This visualization program shows you monthly fatality rate in Japan.
+
+If you change data source url by youself on `main.py`, you can see your country's fatality rate.
+
+## Visualization
+![fatality rate](./images/fatality_rate.png)
+
+## How to install
+
+    pip install -r requirements.txt
+
+## How to run
+
+    python main.py
+
+
+## Data
+```
+            new_case  new_death
+day
+2020-02-29     230.0        5.0
+2020-03-31    1723.0       51.0
+2020-04-30   12135.0      359.0
+2020-05-31    2763.0      476.0
+2020-06-30    1742.0       81.0
+2020-07-31   15779.0       34.0
+```
+
+```
+            fatality_rate(%)
+day
+2020-02-29          2.173913
+2020-03-31          2.959954
+2020-04-30          2.958385
+2020-05-31         17.227651
+2020-06-30          4.649828
+2020-07-31          0.215476
+```
+
+## Source
+- [Wikipedia](https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data)
+- [About this data](https://support.google.com/websearch/answer/9814707?p=cvd19_statistics&hl=en-JP&visit_id=637318352693244221-1670221881&rd=1)
---
+++ main.py
@@ -0,0 +1,111 @@
+import sys
+import os
+import time
+from datetime import datetime,date
+
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+COVID19_URL = "https://en.wikipedia.org/wiki/Template:COVID-19_pandemic_data/Japan_medical_cases_chart"
+CLICKING_ID_PREFIX = 'mw-customcollapsible-'
+CLICKING_ID_PREFIX_CLASS = 'mw-customtoggle-'
+MONTHS = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'jul-l15']
+clicking_ids = [CLICKING_ID_PREFIX + m for m in MONTHS]
+LASTEST_DOWNLOADED_HTML_PATH = "./last_downloaded.html"
+
+def download_html_code():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Version/10.0 Mobile/14C92')
+    driver = webdriver.Chrome("./chromedriver", options=options)
+    driver.set_window_size(600,1000)
+    driver.get(COVID19_URL)
+
+    html_code = driver.page_source
+    driver.close()
+    driver.quit()
+
+    return html_code
+
+def remove_comma(text):
+    return text.replace(',', '')
+
+def check_date(date_text):
+    try:
+        datetime.strptime(date_text,"%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
+
+def get_data_frame(html_code):
+    soup = BeautifulSoup(html_code, "html.parser")
+    df = pd.DataFrame(columns=["day", "new_case", "new_death"])
+    previous_case = 0
+    previous_death = 0
+
+    for cid in clicking_ids:
+        rows = soup.select('table tr[id="%s"]' % cid)
+
+        for r in rows:
+            day = r.select('td:nth-child(1)')[0]
+            case = r.select('td:nth-child(3) span[class="cbs-ibr"]')[0]
+            death = r.select('td:nth-child(4) span[class="cbs-ibr"]')[0]
+            day = day.get_text()
+            case = remove_comma(case.get_text())
+            death =remove_comma(death.get_text())
+            #print(type(day), type(case), type(death))
+            #print(day, case, death)
+
+            if not day or not case or not death:
+                continue
+            if not check_date(day):
+                continue
+            if not case.isdecimal():
+                continue
+            if not death.isdecimal():
+                continue
+
+            case, death  = float(case), float(death)
+            new_case = case - previous_case
+            new_death = death - previous_death
+            day = datetime.strptime(day, "%Y-%m-%d")
+
+            df = df.append({'day': day, 'new_case': new_case, 'new_death': new_death}, ignore_index=True)
+
+            previous_case = case
+            previous_death = death
+
+    return df
+
+def main():
+    html_code = ""
+
+    if os.path.exists(LASTEST_DOWNLOADED_HTML_PATH):
+        with open(LASTEST_DOWNLOADED_HTML_PATH, 'r') as rf:
+            html_code = rf.read()
+    else:
+        html_code = download_html_code()
+        with open(LASTEST_DOWNLOADED_HTML_PATH, 'w') as wf:
+            wf.write(html_code)
+
+    df = get_data_frame(html_code)
+    #print(df)
+
+    df = df.resample(rule="M", on='day').sum()
+    print(df)
+    df.plot()
+
+    df['fatality_rate(%)'] = 100 * df['new_death'] / df['new_case']
+    df = df.drop("new_case", axis=1).drop("new_death", axis=1)
+    print(df)
+    df.plot()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
